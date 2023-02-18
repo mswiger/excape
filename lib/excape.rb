87bbd14d502f1154430a99a8293c35acc641de44
Michael Swiger <michael@swiger.dev>
2023-02-17 21:39:23 -0800

    Initial commit

---
+++ lib/excape.rb
@@ -0,0 +1,17 @@
+class Excape
+  COMMANDS = {
+    "Logout": "swaymsg exit",
+    "Reboot": "systemctl restart",
+    "Reboot Into Windows": "systemctl reboot --boot-loader-entry=auto-windows",
+    "Shutdown": "systemctl poweroff",
+  }.freeze
+
+  def initialize
+    command_list = COMMANDS.keys.join("\n")
+    selected = `echo "#{command_list}" | wofi --show dmenu`.strip!
+    selected_command = COMMANDS[selected.to_sym]
+    msg = "You selected \"#{selected}.\" Are you sure?"
+    button = "Yes, I am sure"
+    `swaynag -t warning -m '#{msg}' -b '#{button}' '#{selected_command}'` if selected
+  end
+end
